86e36fb9082763039f79062f832572bcff7e6589
Ashley Natasha <ashleyotsiula@gmail.com>
2024-11-30 10:17:53 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length):      
+        if length == 0:
+           print([])
+
+        elif length == 1:
+            print([0])
+
+        elif length == 2:
+            print([0,1])
+        
+        else:
+            sequence =[0,1]
+            while len(sequence) < length:
+                sequence.append(sequence[-1] + sequence[-2])
+                
+            print(sequence)
+
+        
+       
+            
+
+        
